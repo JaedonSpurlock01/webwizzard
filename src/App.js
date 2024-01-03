1bacefba6d5149512406d9cecf329c28192838eb
JaedonSpurlock01 <112233517+JaedonSpurlock01@users.noreply.github.com>
2024-01-02 22:23:33 -0800

    bug: trying to fix bug

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
 
+import { IoEnterOutline } from "react-icons/io5";
+import { FaPowerOff } from "react-icons/fa6";
+import { CiSettings } from "react-icons/ci";
+import { MdOutlineHelpOutline } from "react-icons/md";
+import { IoIosGlobe } from "react-icons/io";
+import { FaHistory } from "react-icons/fa";
+
 function App() {
   return (
     <div className="w-[15rem] h-[30rem] bg-[#121212] rounded-lg">
-      <div>
-        
-      </div>
-      <div>
-
-      </div>
-      <div>
-
-      </div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.createElement("div");
+rootElement.id = "my-extension-root";
+document.body.appendChild(rootElement);
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+console.log("TEST!!!!");
