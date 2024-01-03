2adfc061a6a9a178ce9f8c2734add70b40c6fde7
JaedonSpurlock01 <112233517+JaedonSpurlock01@users.noreply.github.com>
2024-01-02 21:02:09 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-[15rem] h-[30rem] bg-[#121212] rounded-lg">
+      <div>
+        
+      </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 }
