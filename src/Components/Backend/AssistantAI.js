732ceb6e89481615e527bb68bfe6acd2b25baf94
Cyphonvoid <yashaswi.kul@gmail.com>
2024-01-10 19:28:35 -0800

    More AI changes

--- src/Components/Backend/AssistantAI.js
+++ src/Components/Backend/AssistantAI.js
@@ -6,19 +6,6 @@ const {GoogleGenerativeAI} = require('@google/generative-ai')
 const API_KEY = "AIzaSyD5LFvK9nIg-JJXI_lpVn4zsVNjUPCZz4w";
 const GOOGLE_AI_MODELS = new GoogleGenerativeAI(API_KEY)
 
-async function run() {
-    // For text-only input, use the gemini-pro model
-    const model = GOOGLE_AI_MODELS.getGenerativeModel({ model: "gemini-pro"});
-  
-    const prompt = "Write a story about a magic backpack."
-  
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-  }
-
-
 
 class GeminiAI {
 
