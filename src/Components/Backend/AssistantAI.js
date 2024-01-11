be206634ee15a2848df96bc6ac90ada808604ebb
Cyphonvoid <yashaswi.kul@gmail.com>
2024-01-10 20:03:05 -0800

    Chat History

--- src/Components/Backend/AssistantAI.js
+++ src/Components/Backend/AssistantAI.js
@@ -1,17 +1,63 @@
-//import { GoogleGenerativeAI } from "@google/generative-ai";
 
+
+const { HarmBlockThreshold, HarmCategory} = require("@google/generative-ai")
 const {GoogleGenerativeAI} = require('@google/generative-ai')
 
 
 const API_KEY = "AIzaSyD5LFvK9nIg-JJXI_lpVn4zsVNjUPCZz4w";
 const GOOGLE_AI_MODELS = new GoogleGenerativeAI(API_KEY)
+const SAFETY_CONFIGURATION = [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
 
+      {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+      }, 
+
+      {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      
+      {
+            category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+            threshold: HarmBlockThreshold.BLOCK_NONE
+      },
+
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE
+      },
+
+]
+
+
+class UserSession{
+
+    constructor(){
+
+    }
+
+
+    LoadConversations(){
+
+    }
+
+};
 
 class GeminiAI {
 
-    constructor(){
+    constructor(safety_config){
         this.__AI_MODEL__ = GOOGLE_AI_MODELS.getGenerativeModel({model: "gemini-pro"});
+        this.__AI_CONVERSATION_SESSION = this.__AI_MODEL__.startChat()
+   
         this.__response = null
+        this.__chat_history = null; //Load the history through the backend
+        /*Safety Ratings*/
+        this.__safety_settings = safety_config; 
     }
 
     async Send(prompt){
@@ -27,9 +73,13 @@ class GeminiAI {
         return this.__response; 
     }
 
+    PrintResponse(){
+        console.log()
+    }
+
 }
 
-let AI = new GeminiAI()
+let AI = new GeminiAI(SAFETY_CONFIGURATION)
 
 AI.Send("How are you today")
 console.log("AI=> ", AI.Recieve())
